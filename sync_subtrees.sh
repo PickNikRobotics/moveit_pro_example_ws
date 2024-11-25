42da05cec9a915ef7f62d0d57c50b5ec2103c615
Michael Wrock <michael.wrock@picknik.ai>
2024-11-25 14:51:31 -0800

    Subtree sync (#19)
    Sync upstream changes
    
    Former-commit-id: 9cfc575f3bfa65c5c0307459635b389ed087d427

--- sync_subtrees.sh
+++ sync_subtrees.sh
@@ -7,6 +7,6 @@ git subtree pull --prefix src/moveit_pro_kinova_configs https://github.com/PickN
 git subtree pull --prefix src/moveit_pro_mobile_manipulation https://github.com/PickNikRobotics/moveit_pro_mobile_manipulation main --squash
 git subtree pull --prefix src/fanuc_sim https://github.com/PickNikRobotics/fanuc_sim main --squash
 git subtree pull --prefix src/external_dependencies/ridgeback https://github.com/sjahr/ridgeback ros2 --squash
-git subtree pull --prefix src/external_dependencies/ros2_robotiq_gripper https://github.com/PickNikRobotics/ros2_robotiq_gripper main --squash
+git subtree pull --prefix src/external_dependencies/ros2_robotiq_gripper https://github.com/PickNikRobotics/ros2_robotiq_gripper humble --squash
 git subtree pull --prefix src/external_dependencies/serial https://github.com/tylerjw/serial.git ros2 --squash
-gis submodule update --recursive --init
+git submodule update --recursive --init
