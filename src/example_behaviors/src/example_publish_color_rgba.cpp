6292feed3f07075d961b85d507480c548a940d40
Michael Wrock <michael.wrock@picknik.ai>
2024-12-05 09:34:23 -0800

    Sync 6.5 to main (#37)

--- src/example_behaviors/src/example_publish_color_rgba.cpp
+++ src/example_behaviors/src/example_publish_color_rgba.cpp
@@ -1,26 +1,26 @@
-#include <example_behaviors/publish_color_rgba.hpp>
+#include <example_behaviors/example_publish_color_rgba.hpp>
 
 namespace example_behaviors
 {
-PublishColorRGBA::PublishColorRGBA(const std::string& name, const BT::NodeConfiguration& config,
+ExamplePublishColorRGBA::ExamplePublishColorRGBA(const std::string& name, const BT::NodeConfiguration& config,
                                    const std::shared_ptr<moveit_studio::behaviors::BehaviorContext>& shared_resources)
   : moveit_studio::behaviors::SharedResourcesNode<BT::SyncActionNode>(name, config, shared_resources)
   , publisher_{ shared_resources_->node->create_publisher<std_msgs::msg::ColorRGBA>("/my_topic", rclcpp::QoS(1)) }
 {
 }
 
-BT::PortsList PublishColorRGBA::providedPorts()
+BT::PortsList ExamplePublishColorRGBA::providedPorts()
 {
   return {};
 }
 
-BT::KeyValueVector PublishColorRGBA::metadata()
+BT::KeyValueVector ExamplePublishColorRGBA::metadata()
 {
-  return { { "subcategory", "Example" },
+  return { { "subcategory", "Example Behaviors" },
            { "description", "Publishes a fixed std_msgs::msg::ColorRGBA message to a topic named \"/my_topic\"" } };
 }
 
-BT::NodeStatus PublishColorRGBA::tick()
+BT::NodeStatus ExamplePublishColorRGBA::tick()
 {
   std_msgs::msg::ColorRGBA color_msg;
   color_msg.r = 0.5;
