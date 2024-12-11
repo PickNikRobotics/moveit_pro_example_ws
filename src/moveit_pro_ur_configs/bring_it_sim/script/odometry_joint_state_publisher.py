7b36408c9ef25220ca33e467ca3ab023d5412737
Henning Kayser <henningkayser@picknik.ai>
2024-12-11 22:34:12 +0000

    Republish odom as joint states
    Former-commit-id: 01718eb9a72552906e22fc57dc75ef061ec8bba9

---
+++ src/moveit_pro_ur_configs/bring_it_sim/script/odometry_joint_state_publisher.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import JointState
+
+class OdometryJointStateRepublisher(Node):
+    """! The Odometry to JointState republisher node.
+
+    Subscribes to Odometry messages, copies the data into JointState messages,
+    which are published to '/joint_states'.
+    """
+
+    def __init__(self, odom_topic, joint_states_topic):
+        """! Initialize Odometry subscriber and JointState publisher.
+
+        @param odom_topic topic name to use for Odometry subscription
+        @param joint_states_topic topic name to use for JointState publishing
+
+        @return Instance of the OdometryJointStateRepublisher node
+        """
+
+        super().__init__('odometry_joint_state_republisher')
+        self.odom_sub_ = self.create_subscription(Odometry, odom_topic, self.odom_callback, 1)
+        self.joint_states_pub_ = self.create_publisher(JointState, joint_states_topic, 1)
+
+    def odom_callback(self, odom_msg):
+        """! Subscription callback to run for incoming Odometry messages
+
+        Each Odometry message is copied into JointState messages and republished 
+        to /joint_states.
+
+        """
+
+        # Get yaw angle from quaternion orientation,
+        # see https://stackoverflow.com/questions/5782658/extracting-yaw-from-a-quaternion
+        q = odom_msg.pose.pose.orientation
+        rotation_yaw = atan2(2.0*(q.y*q.z + q.w*q.x), q.w*q.w - q.x*q.x - q.y*q.y + q.z*q.z);
+
+        # Populate joint state message with x,y,yaw joint dimensions
+        joint_state_msg = JointState()
+        joint_state_msg.name = [
+            "linear_x_joint",
+            "linear_y_joint",
+            "rotational_yaw_joint"
+        ]
+        joint_state_msg.position = [
+            odom_msg.pose.pose.position.x,
+            odom_msg.pose.pose.position.y,
+            rotation_yaw
+        ]
+        self.joint_states_pub_.publish(joint_state_msg)
+
+def main(args=None):
+  rclpy.init(args=args)
+
+  odometry_repub = OdometryJointStateRepublisher('/odom', '/joint_states')
+
+  rclpy.spin(odometry_repub)
+
+  odometry_repub.destroy_node()
+
+  rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
