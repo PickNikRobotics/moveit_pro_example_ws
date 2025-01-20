be9c4f054a8d72aa20bb1409568d7da67bf7c54d
Paul Gesel <paul.gesel@picknik.ai>
2025-01-20 14:13:24 -0700

    run pre-commit and add camera mount to MJCF
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>

--- src/moveit_pro_mobile_manipulation/bring_it_sim/script/odometry_joint_state_publisher.py
+++ src/moveit_pro_mobile_manipulation/bring_it_sim/script/odometry_joint_state_publisher.py
@@ -26,7 +26,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import rclpy
 from rclpy.node import Node
 
