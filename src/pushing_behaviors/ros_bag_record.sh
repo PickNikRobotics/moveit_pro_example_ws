37d52c94ad21cb0831db3a883cec2880895f1dc6
Paul Gesel <paul.gesel@picknik.ai>
2025-02-27 10:55:32 -0700

    Pushing sim v1
    Add basic scene
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    Add keypoint sites to model
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    Add boilerplate for push behavior
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    add friction python script
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    kind of maybe working
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    working demo
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    recorded GIF
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    use moveit_studio::behaviors::SharedResourcesNode<BT::StatefulActionNode>
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    increased precision
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    add ros2 bag record
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    fix hand offset
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    increase finger width
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    Add python script for mcap loader
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    break into functions
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    working generator
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    add joint state interpolation
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    refactor loader
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    Move file out
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    Add second camera
    
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>
    
    Former-commit-id: 8d3dc0cc4d53f77eff477f93bcb34dd1d11d8de1

---
+++ src/pushing_behaviors/ros_bag_record.sh
@@ -0,0 +1,1 @@
+ros2 bag record --max-bag-size 1000000000 -s mcap --compression-mode file --compression-format zstd  /demonstration_indicator /wrist_camera/color /joint_states -o /media/paul/DATA/pushing_data
---
+++ src/pushing_sim/friction.py
@@ -0,0 +1,89 @@
+import cvxpy as cp
+import numpy as np
+
+T = np.array(
+    [
+        [1, 1, 1, 1, 1, 1],  # 2.5
+        [1, 1, 1, 1, 1, 1],
+        [0, 0, 1, 1, 0, 0],  # ~0.8
+        [0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0],  # -2.5
+    ]
+)
+
+
+def get_points(T):
+    points = []
+    com = np.zeros(
+        2,
+    )
+    for y_ind in range(T.shape[0]):
+        for x_ind in range(T.shape[1]):
+            if T[y_ind, x_ind] > 0:
+                arr = np.array(
+                    [x_ind - T.shape[1] / 2 + 0.5, -(y_ind - T.shape[0] / 2) - 0.5]
+                )
+                points.append(arr)
+                com += arr
+
+    return points, com / len(points)
+
+
+points, com = get_points(T)
+
+
+def get_eqs(point, com):
+    diff = np.hstack([point - com, 0.0])
+    rot_axis = np.array([0, 0, 1])
+    rot_vel = np.cross(diff, rot_axis)
+    row_x = [1, 0, rot_vel[0]]  # (p-com)w_x + xd
+    row_y = [0, 1, rot_vel[1]]  # (p-com)w_x + xd
+    return row_x, row_y
+
+
+rows = []
+for point in points:
+    row_x, row_y = get_eqs(point, com)
+    rows.append(row_x)
+    rows.append(row_y)
+
+
+# Define M (linear transformation matrix) and a (target vector)
+M = np.vstack(rows)  # Example M matrix (3x2)
+a = np.zeros(len(rows))  # Example target vector (size 3)
+
+print("M", M)
+
+# Compute Q and c for the QP formulation
+Q = 2 * (M.T @ M)  # Q = 2 * M^T M
+c = -2 * (M.T @ a)  # c = -2 * M^T a
+
+
+con_x, con_y = get_eqs(np.array([0.5, -2.5]), com)
+# A = np.vstack([con_x, con_y])  # Constraint coefficients
+# b = np.array([[-1.0], [0]])  # Constraint bounds
+A = np.vstack([con_x])  # Constraint coefficients
+b = np.array([[-1.0]])  # Constraint bounds
+
+print("A", A)
+
+# Define variables
+# vars to estimate xd, yd, thetad
+x = cp.Variable(3)
+
+# Define objective function (quadratic)
+objective = cp.Minimize(0.5 * cp.quad_form(x, Q) + c @ x)
+
+# Define constraints
+constraints = [A @ x <= b, A @ x >= b]  # x should also be non-negative
+
+# Solve the problem
+prob = cp.Problem(objective, constraints)
+result = prob.solve()
+
+# Output results
+print("Optimal value:", result)
+print("Optimal x:", x.value)
+
+print(A @ x.value)
