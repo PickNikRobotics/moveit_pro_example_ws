9cd7774da5311746f218cded38969d37c1a1d479
Paul Gesel <paul.gesel@picknik.ai>
2025-04-07 14:54:02 -0600

    working integration test
    Signed-off-by: Paul Gesel <paul.gesel@picknik.ai>

--- src/lab_sim/test/test_objective_server_node.py
+++ src/lab_sim/test/test_objective_server_node.py
@@ -9,12 +9,12 @@ from pathlib import Path
 
 import launch
 from launch import LaunchDescription
-from launch_ros.actions import Node
+import launch_ros
 from moveit_studio_utils_py.system_config import (
     SystemConfigParser,
     get_behavior_folder,
     get_config_package,
-    get_config_folder
+    get_config_folder,
 )
 from moveit_studio_rest_api.routers.objective_api.objective_common import (
     parse_objective,
@@ -22,14 +22,17 @@ from moveit_studio_rest_api.routers.objective_api.objective_common import (
 )
 
 from moveit_studio_agent.objective_test_common import (
-    generate_robot_drivers_launch_description, generate_agent_launch_description
+    generate_robot_drivers_launch_description,
+    generate_agent_launch_description,
 )
-from moveit_studio_bridge.launch.launch_descriptions import generate_studio_bridge_launch_description
+from moveit_studio_bridge.launch.launch_descriptions import (
+    generate_studio_bridge_launch_description,
+)
 
 from moveit_studio_sdk_msgs.action import DoObjectiveSequence
 import rclpy
 from rclpy.action import ActionClient
-from rclpy.executors import MultiThreadedExecutor
+from rclpy.node import Node
 from moveit_msgs.msg import MoveItErrorCodes
 
 
@@ -45,7 +48,9 @@ def get_all_objective_paths(system_config_parser: SystemConfigParser) -> list[Pa
     objective_xml_paths = []
     objectives_dirs = system_config_parser.get_objective_library_paths()
     for objectives_dir in objectives_dirs:
-        objective_xml_paths.extend([objectives_dir / f.name for f in objectives_dir.glob("*.xml")])
+        objective_xml_paths.extend(
+            [objectives_dir / f.name for f in objectives_dir.glob("*.xml")]
+        )
 
     return objective_xml_paths
 
@@ -65,7 +70,7 @@ def get_runnable_objectives_xml(objective_xml_paths: list[Path]) -> list[str]:
 def generate_rest_api_launch_description():
     return LaunchDescription(
         [
-            Node(
+            launch_ros.actions.Node(
                 package="moveit_studio_rest_api",
                 executable="web_server_node",
                 name="web_server_node",
@@ -75,71 +80,153 @@ def generate_rest_api_launch_description():
     )
 
 
+class ActionData:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        self.result = None
+        self.goal_handle = None
+        self.send_goal_future = None
+        self.get_result_future = None
+        self.cancel_future = None
+        self.cancel_response = None
+
+    def reset(self):
+        self.result = None
+        self.goal_handle = None
+        self.send_goal_future = None
+        self.get_result_future = None
+        self.cancel_future = None
+
+
+class DoObjectiveSequenceResource:
+    def __init__(self, action_data: ActionData):
+        self.node = Node("do_objective_node")
+        self.do_objective_sequence_client = ActionClient(
+            self.node, DoObjectiveSequence, "/do_objective"
+        )
+        self.action_data = action_data
+
+    def wait_for_server(self):
+        self.do_objective_sequence_client.wait_for_server()
+
+    def send_goal(self, goal):
+        self.action_data.send_goal_future = self.do_objective_sequence_client.send_goal_async(goal)
+
+    def cancel_goal(self):
+        if self.action_data.goal_handle is not None:
+            self.action_data.cancel_future = self.action_data.goal_handle.cancel_goal_async()
+            self.action_data.cancel_future.add_done_callback(self._cancel_done_callback)
+
+    def wait_for_cancel_response(self, timeout_sec):
+        self.callback_on_future_complete(self.action_data.cancel_future, self._cancel_done_callback,
+                                         timeout_sec=timeout_sec)
+
+    def wait_for_goal_response(self, timeout_sec):
+        self.callback_on_future_complete(self.action_data.send_goal_future, self._goal_response_callback,
+                                         timeout_sec=timeout_sec
+                                         )
+
+    def wait_for_result(self, timeout_sec):
+        self.callback_on_future_complete(self.action_data.get_result_future, self._get_result_callback,
+                                         timeout_sec=timeout_sec)
+
+    def callback_on_future_complete(self, future, callback, timeout_sec=-1):
+        current_time = self.node.get_clock().now()
+        while not future.done():
+            if timeout_sec - (self.node.get_clock().now() - current_time).to_msg().sec == 0:
+                return
+            rclpy.spin_once(self.node)
+
+        callback(future)
+
+    def _cancel_done_callback(self, future):
+        self.action_data.cancel_response = future.result()
+
+    def _get_result_callback(self, future):
+        self.action_data.result = future.result().result
+
+    def _goal_response_callback(self, future):
+        self.action_data.goal_handle = future.result()
+        if not self.action_data.goal_handle.accepted:
+            return
+
+        self.action_data.get_result_future = (
+            self.action_data.goal_handle.get_result_async()
+        )
+        self.action_data.get_result_future.add_done_callback(self._get_result_callback)
+
+
 @pytest.fixture(scope="module")
-def do_objective_sequence_fixture():
-    print("Setup")
-    all_launch_services_descriptions = [generate_robot_drivers_launch_description(),
-                                        generate_agent_launch_description(), generate_rest_api_launch_description(),
-                                        generate_studio_bridge_launch_description()
-                                        ]
+def do_objective_sequence_resource():
+    all_launch_services_descriptions = [
+        generate_robot_drivers_launch_description(),
+        generate_agent_launch_description(),
+        generate_rest_api_launch_description(),
+        generate_studio_bridge_launch_description(),
+    ]
 
     moveit_pro_launch_service = launch.LaunchService()
     moveit_pro_launch_description = LaunchDescription(all_launch_services_descriptions)
     moveit_pro_launch_service.include_launch_description(moveit_pro_launch_description)
 
-    def worker():
+    def run_launch_file():
         _ = moveit_pro_launch_service.run()
 
-    p = multiprocessing.Process(target=worker)
+    p = multiprocessing.Process(target=run_launch_file)
     p.start()
 
     if not rclpy.ok():
         rclpy.init()
 
-    node = rclpy.node.Node("do_objective_node")
-    do_objective_sequence_client = ActionClient(node, DoObjectiveSequence, "/do_objective")
-    do_objective_sequence_client.wait_for_server()
-    print("server ready")
-
-    yield node, do_objective_sequence_client
+    time.sleep(15)
+    yield None
     os.kill(p.pid, signal.SIGINT)
     p.join()
-    print("Teardown")
 
 
 @pytest.mark.parametrize("objective_xml", get_runnable_config_objectives("lab_sim"))
-def test_objective_server(objective_xml, do_objective_sequence_fixture):
-    """Tests the sum of the example data."""
-    node, do_objective_sequence_client = do_objective_sequence_fixture
+def test_objective_server(
+        objective_xml, do_objective_sequence_resource: DoObjectiveSequenceResource
+):
+    time.sleep(1)
+
+    cancel_objectives = {"Teleoperate", "Joint Diagnostic", 'Segment Point Cloud from Clicked Point', 'Place Object',
+                         'Loop Detect AprilTag', 'Grasp Pose Tuning With April Tag', 'Grasp Object from Text Prompt',
+                         'Grasp Planning', '3 Waypoints Pick and Place', 'Pick And Place Example',
+                         'Record and Replay Scanning Motion', 'Plan and Save Trajectory', 'Cycle Between Waypoints',
+                         'Pick April Tag Labeled Object With Approval'}
+    skip_objectives = {'Joint Diagnostic'}
     objective = parse_objective_xml(objective_xml)
+
+    if objective.id in skip_objectives:
+        return
 
     goal = DoObjectiveSequence.Goal()
     goal.objective_name = objective.id
 
-    goal_handle = do_objective_sequence_client.send_goal_async(goal)
-    result_list = []
+    # send the action goal to run the objective
+    action_data = ActionData()
+    action_data.reset()
+    do_objective_sequence_resource = DoObjectiveSequenceResource(action_data)
+    do_objective_sequence_resource.wait_for_server()
+    do_objective_sequence_resource.send_goal(goal)
 
-    def get_result_callback(future):
-        result = future.result().result
-        result_list.append(result.error_code == MoveItErrorCodes.SUCCESS)
+    # Wait a few seconds for the response. The response indicates whether the request was accepted or rejected.
+    do_objective_sequence_resource.wait_for_goal_response(5)
+    assert action_data.goal_handle.accepted
 
-
-    def goal_response_callback(future):
-        goal_handle = future.result()
-        if not goal_handle.accepted:
-            node.get_logger().info('Goal rejected :(')
-            return
-
-        node.get_logger().info('Goal accepted :)')
-
-        get_result_future = goal_handle.get_result_async()
-        get_result_future.add_done_callback(get_result_callback)
-
-
-    goal_handle.add_done_callback(goal_response_callback)
-
-    time.sleep(5)
-    rclpy.spin_until_future_complete(node, goal_handle, timeout_sec=10)
-    del goal_handle
-
-    assert len(objective_xml) > 0
+    if not objective.id in cancel_objectives:
+        # Wait for the objective to finish
+        do_objective_sequence_resource.wait_for_result(90)
+        assert action_data.result.error_code.val == MoveItErrorCodes.SUCCESS
+    else:
+        time.sleep(10)
+        do_objective_sequence_resource.cancel_goal()
+        do_objective_sequence_resource.wait_for_cancel_response(5)
+        assert action_data.cancel_response.return_code == 0
