fe13ea887d5c1465a3edde0013239c77a8c72c94
Giovanni Remigi <giovanni.remigi@picknik.ai>
2025-08-21 21:03:23 +0200

    Reducing Franka workspace build time.

--- src/moveit_pro_franka_configs/franka_dual_arm_sim/scripts/svg_to_yaml.py
+++ src/moveit_pro_franka_configs/franka_dual_arm_sim/scripts/svg_to_yaml.py
@@ -1,11 +1,46 @@
 #!/usr/bin/env python3
+#
+# Copyright 2025 PickNik Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the PickNik Inc. nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""
+SVG to Waypoints Converter
+
+This module converts SVG paths into 3D waypoints formatted as YAML PoseStamped messages.
+It extracts paths from SVG files, samples points along the paths based on specified density,
+and generates waypoints with appropriate z-clearance for transitions between paths.
+"""
 
 import argparse
-from svgpathtools import svg2paths
-import numpy as np
 import copy
-
+import numpy as np
 import pygame
+from svgpathtools import svg2paths
 
 
 def write_waypoints_to_file(waypoints, filename, scale=1.0):
