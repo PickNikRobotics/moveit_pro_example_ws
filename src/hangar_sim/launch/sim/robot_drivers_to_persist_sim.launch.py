91bb291d9ce2cf40c61055f0fc4875cfb8096930
Kristen Such <suchkristen1@gmail.com>
2026-02-10 16:48:55 -0700

    adding IMU fuse plugin to hangar_sim

--- src/hangar_sim/launch/sim/robot_drivers_to_persist_sim.launch.py
+++ src/hangar_sim/launch/sim/robot_drivers_to_persist_sim.launch.py
@@ -41,13 +41,11 @@ from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import (
     LaunchConfiguration,
-    PathJoinSubstitution,
     PythonExpression,
 )
 from launch_ros.actions import Node
 from launch_ros.actions import PushRosNamespace
 from launch_ros.descriptions import ParameterFile
-from launch_ros.substitutions import FindPackageShare
 from nav2_common.launch import RewrittenYaml, ReplaceString
 
 
@@ -284,17 +282,6 @@ def generate_launch_description():
         output="log",
     )
 
-    # Fuse state estimator for mobile base localization
-    hangar_sim_pkg = FindPackageShare("hangar_sim")
-    fuse_state_estimator = Node(
-        package="fuse_optimizers",
-        executable="fixed_lag_smoother_node",
-        name="state_estimator",
-        parameters=[
-            PathJoinSubstitution([hangar_sim_pkg, "config", "fuse", "fuse.yaml"])
-        ],
-        output="screen",
-    )
 
     # Create the launch description and populate
     ld = LaunchDescription()
@@ -326,6 +313,5 @@ def generate_launch_description():
     ld.add_action(static_tf_map_to_odom)
     ld.add_action(odom_to_joint_state_repub)
     ld.add_action(odom_qos_relay)
-    ld.add_action(fuse_state_estimator)
 
     return ld
